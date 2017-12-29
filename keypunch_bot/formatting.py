589b778d848131d75712279d855a23ee346ec296
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-12-29 18:17:10 +0300

    Switched to Pillow for drawing images

--- keypunch_bot/formatting.py
+++ keypunch_bot/formatting.py
@@ -1,18 +1,21 @@
 import math
-import cairo
+from PIL import Image
+import aggdraw
 
 
 class Formatter:
+    num_columns = 80
+    
     def __init__(self):
         pass
 
-    def rows_numbers(self):
+    def row_numbers(self):
         yield 12
         yield 11
         for i in range(0, 10):
             yield i
 
-    def format(self, codes):
+    def format(self, codes, text):
         raise Exception("Formatter class cannot be used")
 
 
@@ -31,67 +34,129 @@ class TextFormatter(Formatter):
         return result
 
 
-class ImageFormatter(Formatter):
-    width = 7.75
-    height = 3.25
-    dpi = 96
+class GraphicsFormatter(Formatter):
+    dpi = 88
+    width = 7.75 * dpi
+    height = 3.25 * dpi
+    
     padding = 10
-    corner_radius = 0.22
+    corner_radius = 0.22 * dpi
+    corner_cut_width = 0.23 * dpi
+    corner_cut_height = 0.42 * dpi
     
-    def draw_border(self, ctx):
-        ctx.move_to(0.23, 0)
-        ctx.line_to(0, 0.42)
-        ctx.line_to(0, self.height - self.corner_radius)
-        ctx.arc_negative(self.corner_radius, self.height - self.corner_radius, self.corner_radius, math.pi, math.pi / 2)
-        ctx.line_to(self.width - self.corner_radius, self.height)
-        ctx.arc_negative(self.width - self.corner_radius, self.height - self.corner_radius, self.corner_radius, math.pi / 2, 0)
-        ctx.line_to(self.width, self.corner_radius)
-        ctx.arc_negative(self.width - self.corner_radius, self.corner_radius, self.corner_radius, 0, -math.pi / 2)
-        ctx.close_path()
+    padding_left = int(0.24 * dpi)
+    padding_top = int(0.195 * dpi)
+    spacing_horizontal = 3
+    spacing_vertical = 11
+    text_y = -1
+    
+    column_numbers_y = [dpi * 0.81, dpi * 3.05]
+    
+    hole_width = 5
+    hole_height = 11
+    
+    def to_px(self, *xy,  half_px=True):
+        a = 0.5 if half_px else 0
+        return tuple(int(x + self.padding) + a for x in xy)
+    
+    
+class PILFormatter(GraphicsFormatter):
+    
+    def __init__(self):
+        self.text_font = aggdraw.Font("black", "./fonts/Oswald-Regular.ttf", 10)
+        self.row_number_font = aggdraw.Font("black", "./fonts/Oswald-Regular.ttf", 10)
+        self.col_number_font = aggdraw.Font("black", "./fonts/Oswald-Light.ttf", 7)
+    
+    def create(self):
+        im = Image.new("RGBA", (int(self.width + 2 * self.padding), 
+                                int(self.height + 2 * self.padding)))
+        draw = aggdraw.Draw(im)
+        return im, draw
+    
+    def draw_background(self, draw):
+        """ 
+        Drawing a background of a punched card.
+        This method assumes that corner_cut_width > corner_radius and 
+            corner_cut_height > corner_radius"""
+        draw.setantialias(False)
+        b = aggdraw.Brush("#eeeeee")
+        draw.rectangle(self.to_px(0, self.corner_cut_height, self.width, self.height - self.corner_radius), None, b)
+        draw.rectangle(self.to_px(self.corner_radius, self.height - self.corner_radius, 
+                                  self.width - self.corner_radius, self.height), None, b)
+        draw.rectangle(self.to_px(self.corner_cut_width, self.corner_radius, self.width, self.corner_cut_height), None, b)
+        draw.rectangle(self.to_px(self.corner_cut_width, 0, self.width - self.corner_radius, self.corner_radius), None, b)
+        draw.polygon(self.to_px(self.corner_cut_width, 0, 0, self.corner_cut_height, 
+                                self.corner_cut_width, self.corner_cut_height), None, b)
+        draw.pieslice(self.to_px(0, self.height - 2 * self.corner_radius, 2 * self.corner_radius, self.height),
+                      180, 270, None, b)
+        draw.pieslice(self.to_px(self.width - 2 * self.corner_radius, self.height - 2 * self.corner_radius, 
+                      self.width, self.height), 270, 0, None, b)
+        draw.pieslice(self.to_px(self.width - 2 * self.corner_radius, 0, self.width, 2 * self.corner_radius), 0, 90, 
+                      None, b)
+
+    def draw_outline(self, draw):
+        draw.setantialias(True)
+        p = aggdraw.Pen("black", 1)
+        draw.line(self.to_px(self.width - self.corner_radius, 0, self.corner_cut_width, 0, 
+                             0, self.corner_cut_height, 0, self.height - self.corner_radius), p)
+        draw.arc(self.to_px(0, self.height - 2 * self.corner_radius, 2 * self.corner_radius, self.height),
+                 180, 270, p)
+        draw.line(self.to_px(self.corner_radius, self.height, self.width - self.corner_radius, self.height), p)
+        draw.arc(self.to_px(self.width - 2 * self.corner_radius, self.height - 2 * self.corner_radius, 
+                            self.width, self.height), 270, 0, p)
+        draw.line(self.to_px(self.width, self.height - self.corner_radius, self.width, self.corner_radius), p)
+        draw.arc(self.to_px(self.width - 2 * self.corner_radius, 0, self.width, 2 * self.corner_radius), 0, 90, p)
+    
+    def draw_text_middle(self, draw, rect, text, font):
+        width, height = draw.textsize(text, font)
+        draw.text(self.to_px(rect[0] + (rect[2] - rect[0] - width) / 2, 
+                             rect[1] + (rect[3] - rect[1] - height) / 2 - 1), text, font)
+    
+    def draw_numbers(self, draw, codes):
+        row_numbers = list(self.row_numbers())
+        draw.setantialias(False)
         
-        ctx.stroke_preserve()
-        ctx.set_source_rgb (0.9, 0.9, 0.9)
-        ctx.fill()
-    
-    def format(self, codes):
-        surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, int(self.width * self.dpi + 2 * self.padding), 
-                                      int(self.height * self.dpi + 2 * self.padding))
-        ctx = cairo.Context (surface)
-
-        ctx.set_source_rgb (1, 1, 1)
-        ctx.rectangle (0, 0, self.width * self.dpi + 2 * self.padding, self.height * self.dpi + 2 * self.padding)
-        ctx.fill ()
-
-        ctx.scale(self.dpi, self.dpi)
-        ctx.translate((self.padding + 0.5) / self.dpi, (self.padding + 0.5) / self.dpi)
-
-        ctx.set_source_rgb (0, 0, 0)
-        ctx.set_line_width (1 / self.dpi)
-
-        self.draw_border(ctx)
-
-        ctx.set_font_size(0.1)
-        current_symbol = set()
-        rows = list(self.rows_numbers())
-        for column in range(80):
-            if current_symbol is not None:
+        codes_ended = False
+        for column in range(self.num_columns):
+            if not codes_ended:
                 try:
-                    current_symbol = next(codes)
+                    indices = next(codes)
                 except StopIteration:
-                    current_symbol = None
-                
+                    codes_ended = True
+                    
             for row in range(12):
-                if current_symbol is not None and rows[row] in current_symbol:
-                    ctx.set_source_rgb (0, 0, 0)
-                    ctx.rectangle (0.238 + 0.0909 * column, 0.31 + 0.25 * row, 0.06, -0.12)
-                    ctx.fill ()
-                else:
-                    ctx.move_to(0.238 + 0.0909 * column, 0.29 + 0.25 * row)
-                    ctx.show_text(" " if row < 2 else str(row - 2))
-
-        ctx.set_font_size(0.071)
-        for y in [0.911, 3.13]:
-            for column in range(80):
-                ctx.move_to(0.238 + 0.0909 * column, y)
-                ctx.show_text(str(column + 1))
-        return surface
+                rect = [self.padding_left + (self.hole_width + self.spacing_horizontal) * column, 
+                        self.padding_top + (self.hole_height + self.spacing_vertical) * row]
+                rect += [rect[0] + self.hole_width, rect[1] + self.hole_height]
+                if not codes_ended and row_numbers[row] in indices:
+                    draw.rectangle(self.to_px(rect[0] + 1, rect[1], rect[2] + 1, rect[3], 
+                                              half_px=False), aggdraw.Brush("black"))
+                elif row >= 2:
+                    self.draw_text_middle(draw, rect, str(row - 2), self.row_number_font)
+    
+    def draw_calumn_numbers(self, draw, y):
+        x = self.padding_left
+        for i in range(self.num_columns):
+            label = str(i + 1)
+            width, _ = draw.textsize(label, self.col_number_font)
+            draw.text(self.to_px(x + (self.hole_width - width) / 2, y), label, self.col_number_font)
+            x += self.hole_width + self.spacing_horizontal
+    
+    def draw_text(self, draw, text):
+        for i, c in enumerate(text):
+            x = self.padding_left + i * (self.hole_width + self.spacing_horizontal)
+            width, _ = draw.textsize(c, self.text_font)
+            draw.text(self.to_px(x + (self.hole_width - width) / 2, self.text_y), c, self.text_font)
+    
+    def format(self, codes, text, fobj, image_format="png"):
+        image, draw = self.create()
+        self.draw_background(draw)
+        self.draw_outline(draw)
+        self.draw_numbers(draw, codes)
+        for column_number_y in self.column_numbers_y:
+            self.draw_calumn_numbers(draw, column_number_y)
+        if text is not None:
+            self.draw_text(draw, text)
+        draw.flush()
+        del draw
+        image.save(fobj, image_format)
--- keypunch_bot/puncher.py
+++ keypunch_bot/puncher.py
@@ -1,10 +1,11 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-from yaml import load
+import yaml
 import io
+from pprint import pprint
 
 from encoding import Encoder
-from formatting import ImageFormatter
+from formatting import PILFormatter
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -18,6 +19,12 @@ def start(bot, update):
     update.message.reply_text('Welcome!')
 
 
+def text_command(bot, update, args, chat_data):
+    if len(args) > 1:
+        update.message.reply_text("To many arguments")
+    chat_data["cmd"] = args
+
+
 def generate(bot, update):
     if update.edited_message is None:
         message = update.message
@@ -25,9 +32,8 @@ def generate(bot, update):
         message = update.edited_message
     
     for card in encoder.encode(message.text):
-        surface = formatter.format(card)
         stream = io.BytesIO()
-        surface.write_to_png(stream)
+        formatter.format(card, None, stream)
         stream.seek(0)
         bot.send_photo(message.chat_id, photo=stream)
         stream.close()
@@ -40,17 +46,18 @@ def error(bot, update, error):
 def main():
     global encoder, formatter
     with open("config.yaml") as file:
-        config = load(file)
+        config = yaml.load(file)
 
     with open("keycodes.yaml") as file:
-        encoder = Encoder(load(file))
-    formatter = ImageFormatter()
+        encoder = Encoder(yaml.load(file))
+    formatter = PILFormatter()
 
     updater = Updater(config["api_key"])
 
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("text", text_command, pass_args=True, pass_chat_data=True))
     dp.add_handler(MessageHandler(Filters.text, generate, edited_updates=True))
 
     dp.add_error_handler(error)
