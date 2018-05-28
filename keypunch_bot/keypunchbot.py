f90a3eecdc67ce73457b03b9d3964e1506b3ddbf
Vlad Poletaev <poletaev.vladislav@gmail.com>
2018-05-28 21:03:09 +0300

    Added licensing information

--- keypunch_bot/keypunchbot.py
+++ keypunch_bot/keypunchbot.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright 2017, 2018 Vladislav Poletaev
+#
+# This file is part of KeyunchBot.
+#
+# KeyunchBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# KeypunchBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with KeypunchBot. If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 from functools import wraps
