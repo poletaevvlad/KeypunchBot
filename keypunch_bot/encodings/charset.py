733e66ee30e0816c627b982f76cd127607858817
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-02-02 19:14:05 +0300

    Implemented CharacterSet class

---
+++ keypunch_bot/encodings/charset.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017, 2018, 2020 Vladislav Poletaev
+#
+# This file is part of KeyunchBot.
+#
+# KeyunchBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# KeypunchBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with KeypunchBot. If not, see <http://www.gnu.org/licenses/>.
+
+import enum
+from typing import Dict, List
+from dataclasses import dataclass
+
+
+class EncodingType(enum.Enum):
+    PUNCHCARD = "punchcard"
+    TAPE = "tape"
+
+
+@dataclass
+class CharacterEntry:
+    activation: List[int]
+    codes: List[int]
+
+    @property
+    def needs_activation(self) -> bool:
+        return len(self.activation) != 0
+
+
+class CharacterSet:
+    def __init__(self, name: str, charset_type: EncodingType):
+        self.name: str = name
+        self.type: EncodingType = charset_type
+        self._chars: Dict[str, CharacterEntry] = {}
+
+    def add_characters(self, characters: Dict[str, List[int]],
+                       activation: List[int] = None):
+        if activation is None:
+            activation = []
+        for charset, codes in characters.items():
+            entry = CharacterEntry(activation=activation, codes=codes)
+            for char in charset:
+                self._chars[char] = entry
+
+    def __getitem__(self, char: str) -> CharacterEntry:
+        return self._chars[char]
---
+++ tests/encodings/test_charset.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017, 2018, 2020 Vladislav Poletaev
+#
+# This file is part of KeyunchBot.
+#
+# KeyunchBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# KeypunchBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with KeypunchBot. If not, see <http://www.gnu.org/licenses/>.
+
+from keypunch_bot.encodings import CharacterSet, EncodingType
+
+
+def test_creating():
+    charset = CharacterSet("Charset", EncodingType.PUNCHCARD)
+    charset.add_characters(dict(a=[1, 2, 3], b=[4, 6]))
+    charset.add_characters(dict(c=[7]), activation=[15])
+
+    assert charset.name == "Charset"
+    assert charset.type == EncodingType.PUNCHCARD
+
+    assert charset["a"].codes == [1, 2, 3]
+    assert not charset["a"].needs_activation
+
+    assert charset["c"].codes == [7]
+    assert charset["c"].needs_activation
+    assert charset["c"].activation == [15]
