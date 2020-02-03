ec2ca0184e78c7b6caa56381c2fce7ee8a9f5ac6
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-02-03 14:53:05 +0300

    Loading localization files from the filesystem

---
+++ keypunch_bot/i18n/manager.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017, 2018, 2020 Vladislav Poletaev
+#
+# This file is part of KeyunchBot.
+#
+# KeyunchBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# KeypunchBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with KeypunchBot. If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Dict
+from pathlib import Path
+import yaml
+from .language import Language, StringsLanguage, NoMessageLanguage
+
+
+def load_language(fallback: Language, path: Path) -> Language:
+    with path.open() as file:
+        messages = yaml.load(file, yaml.CLoader)
+    return StringsLanguage(fallback, messages)
+
+
+class TranslationManager:
+    def __init__(self, default: str, languages: Dict[str, Language]):
+        self.default = default
+        self.languages = languages
+
+    @staticmethod
+    def load(location: Path, *, default: str) -> "TranslationManager":
+        default_path = location / (default + ".yaml")
+        if not default_path.is_file():
+            raise FileNotFoundError("Cannot open default language file")
+        default_lang = load_language(NoMessageLanguage(), default_path)
+        languages: Dict[str, Language] = {default: default_lang}
+        for filepath in location.iterdir():
+            if filepath.suffix != ".yaml":
+                continue
+            name = filepath.stem
+            if name != default:
+                languages[name] = load_language(default_lang, filepath)
+        return TranslationManager(default, languages)
---
+++ tests/i18n/test_manager.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017, 2018, 2020 Vladislav Poletaev
+#
+# This file is part of KeyunchBot.
+#
+# KeyunchBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# KeypunchBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with KeypunchBot. If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+import pytest
+from keypunch_bot.i18n import TranslationManager
+
+
+def test_loading(tmpdir):
+    temp_path = Path(tmpdir)
+    (temp_path / "en.yaml").write_text("a: A-en\nb: B-en\n")
+    (temp_path / "ru.yaml").write_text("a: A-ru\n")
+    (temp_path / "uk.yaml").write_text("b: B-uk\n")
+
+    manager = TranslationManager.load(temp_path, default="en")
+    assert len(manager.languages) == 3
+    assert set(manager.languages.keys()) == {"en", "ru", "uk"}
+    assert manager.languages["en"]["a"] == "A-en"
+    assert manager.languages["en"]["c"] == "c"
+    assert manager.languages["ru"]["a"] == "A-ru"
+    assert manager.languages["ru"]["b"] == "B-en"
+    assert manager.languages["ru"]["c"] == "c"
+
+
+def test_loading_no_default(tmpdir):
+    with pytest.raises(FileNotFoundError) as exception:
+        TranslationManager.load(Path(tmpdir), default="en")
+    assert str(exception.value) == "Cannot open default language file"
